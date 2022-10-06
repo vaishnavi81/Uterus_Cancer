faab14eb806b113409d96e0c460293956f5bf3a0
vaishnavi81 <91477342+vaishnavi81@users.noreply.github.com>
2022-10-06 13:12:51 +0530

    Add files via upload

---
+++ Z_scoreU.R
@@ -0,0 +1,21 @@
+library(ComplexHeatmap)   #importing library
+library(matrixStats) 
+
+data=source("logcpm.rds")
+
+data2=function(F_data){        #create_function2 and load log2cpm data into it
+  for (i in 1:ncol(F_data)){
+    z_score = (F_data - rowMeans(F_data))/rowSds(as.matrix(F_data))[row(F_data)]
+    
+  }   
+  
+  z_score[is.na(z_score)]=0  
+  z_score#calculate Z_score
+  zscore = as.matrix(z_score)
+  Heatmap(zscore)
+  return(Heatmap(zscore[1:20]))                   #calculate the heatmap
+  
+}
+pdf('data1.pdf',width=10,height=10)
+dev.off()
+data2(F_data)
